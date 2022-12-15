87399f28a45033cf784e65339fd020bcc607178d
Byron Christian Buitre <bbuitre08@gmail.com>
2022-12-15 08:48:41 +0000

    Upload New File

---
+++ Create.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+<div class = "container">
+    <nav>
+        <h3 class = "logo">MiniBlog</h3>
+        <h1 href = "#" class = "btn">Home</h1>
+    </nav>
+</div>
+<div class = "create_full">
+    <form action = "" method = "post">
+        <div class = "infos">
+            <p>Create a Post!</p>
+            <input type="text" name="name" required placeholder="Enter Title" maxlength="50">
+            <input type="text" name="name" required placeholder="Enter Content" maxlength="500">
+        </div>
+        <div class = "post_button">
+        <input type="submit" name="submit" value = "POST" maxlength="500">
+    </form>
+</div>
+
+
+</body>
+
+</html>
